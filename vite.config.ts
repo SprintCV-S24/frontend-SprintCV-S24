c2cf889a88dce58d86346c489720c40fdde6a4cf
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-02-04 22:50:49 -0600

    adds boilerplate and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       // Proxy /api requests to our express server
