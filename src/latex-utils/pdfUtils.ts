e659636617be094289166d5e1abb7a7f589ed018
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-02-23 16:08:26 -0600

    implements messy util functions to allow for rendering pdf (needs refining)

---
+++ src/latex-utils/pdfUtils.ts
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import * as pdfjsLib from "pdfjs-dist";
+
+export const pdfInit = () => {
+  if (typeof window === "undefined" || !("Worker" in window)) {
+    throw new Error("Web Workers not supported in this environment.");
+  }
+  pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.mjs`;
+};
+
+export const usePdfRenderer = (
+  blob: Blob | null,
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>,
+) => {
+  useEffect(() => {
+    if (blob) {
+      const fileURL = URL.createObjectURL(blob);
+      const loadingTask = pdfjsLib.getDocument(fileURL);
+
+      loadingTask.promise
+        .then((pdf) => {
+          pdf.getPage(1).then((page) => {
+            const canvas = canvasRef.current;
+            if (canvas != null) {
+              const viewport = page.getViewport({ scale: 1.5 });
+              const context = canvas.getContext("2d");
+              canvas.height = viewport.height;
+              canvas.width = viewport.width;
+              if (context != null) {
+                const renderContext = {
+                  canvasContext: context,
+                  viewport: viewport,
+                };
+                page.render(renderContext);
+              }
+            }
+          });
+        })
+        .catch((err) => {
+          console.error("Error during PDF loading or rendering: ", err);
+        });
+
+      // Cleanup
+      return () => URL.revokeObjectURL(fileURL);
+    }
+  }, [blob]);
+};
