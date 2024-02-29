6e73273d0769ab4ca9a07a2619733d98e4602d47
Dana Izadpanah <danaizad8624@gmail.com>
2024-02-29 17:55:28 -0600

    Created the latex versoin of heading, experiece heading, ExperinItems-- fully tested, there is a problem iwht having % in bullet points it will ruin latex

--- src/latexUtils/latexString.ts
+++ src/latexUtils/latexString.ts
@@ -1,5 +1,65 @@
 
+//you don't care about user and itemName fields, but all other fields should be in the latex you generate
 
+
+/*  ------------------------------------------------- */
+/*  -------------------Heading----------------------- */
+/*  ------------------------------------------------- */
+interface HeadingItem {
+    item: string;
+    href: string | null;
+  }
+  
+  // Interface for Contact details document
+  export interface HeadingType {
+      user: string;
+      itemName: string;
+      name: string;
+      items: HeadingItem[];
+  }
+
+// Generating the full LaTeX for the Experience Section
+export const generateHeaderLatex= (activityObj: HeadingType) => {
+    let headerLatex = `\\begin{center}\n`;
+    headerLatex += `\\textbf{\\Huge \\scshape ${activityObj.name}} \\\\ \\vspace{1pt}\n`;
+    headerLatex += `\\small `;
+
+    // Iterate over each item to append it to the LaTeX string
+    activityObj.items.forEach((item, index) => {
+        if (item.href) {
+        headerLatex += `\\href{${item.href}}{\\underline{${item.item}}}`;
+        } else {
+        headerLatex += item.item;
+        }
+
+        // Add a separator if it's not the last item
+        if (index < activityObj.items.length - 1) {
+        headerLatex += ` $|$ `;
+        }
+    });
+
+    headerLatex += `\n\\end{center}`;
+    return headerLatex;
+}
+
+// testing the header
+
+export const fakeHeading: HeadingType = {
+    user: "John",
+    itemName: "DOE",
+    name: "This Is My Name",
+    items: [
+        { item: "Hello", href: null }, 
+        { item: "meow", href: null }, 
+        { item: "dog.com", href: "http://dog.com" }
+    ]
+};
+
+
+
+/*  ------------------------------------------------- */
+/*  -------------------Experience-------------------- */
+/*  ------------------------------------------------- */
 export interface ExperienceType {
 	user: string;
 	itemName: string;
@@ -10,22 +70,8 @@ export interface ExperienceType {
     location: string;
 }
 
-//you don't care about user and itemName fields, but all other fields should be in the latex you generate
-
-// \resumeSubheading
-// {Undergraduate Research Assistant}{June 2020 -- Present}
-// {Texas A\&M University}{College Station, TX}
-// \resumeItemListStart
-//   \resumeItem{Developed a REST API using FastAPI and PostgreSQL to store data from learning management systems}
-//   \resumeItem{Developed a full-stack web application using Flask, React, PostgreSQL and Docker to analyze GitHub data}
-//   \resumeItem{Explored ways to visualize GitHub collaboration in a classroom setting}
-// \resumeItemListEnd
-
-
-/*  -------------------Experience-------------------- */
-
 // Generating the full LaTeX for the Experience Section
-const generateExperienceLatex = (activityObj: ExperienceType) => {
+export const generateExperienceLatex = (activityObj: ExperienceType) => {
     let latexString = `\\resumeSubheading{${activityObj.title}}{${activityObj.subtitle}}{${activityObj.location}}{${activityObj.date}}
     \\resumeItemListStart
     `;
@@ -38,20 +84,25 @@ const generateExperienceLatex = (activityObj: ExperienceType) => {
     return latexString;
 }
 
-// This is a potential solution to having experience header in latex string object that 
-// is returned 
-const generateExperienceSection = (experiences: ExperienceType[]): string => {
-    let section = `%-----------EXPERIENCE-----------
-\\section{Experience}
-\\resumeSubHeadingListStart
-`;
-    experiences.forEach(exp => {
-        section += generateExperienceLatex(exp) + "\n";
-    });
-    section += `\\resumeSubHeadingListEnd
-`;
-    return section;
+// Generating the full LaTeX for the Experience Section
+export const generateExperienceHeaderLatex = () => {
+    let latexString = `\\Experience`;
+    return latexString;
+}
+
+export const fakeExperience: ExperienceType = {
+    user: "Jane Doe", // Irrelevant for our test
+    itemName: "Resume Item", // Irrelevant for our test
+    bullets: [
+        "Developed and implemented efficient algorithms",
+        "Collaborated with a cross-functional team",
+        "Improved system performance by 20%"
+    ],
+    title: "Software Engineer",
+    subtitle: "Acme Corporation",
+    date: "2022 - 2023",
+    location: "Los Angeles, CA"
 };
 
-
-/*  -------------------Education-------------------- */
+const generatedLatex = generateExperienceLatex(fakeExperience);
+console.log(generatedLatex);
