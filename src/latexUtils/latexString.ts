1d81c6ffb73be4472acc5baa753ae090cbc14ca8
Dana Izadpanah <danaizad8624@gmail.com>
2024-03-02 11:43:05 -0600

    The generateHeaderLatex is now complete, it sanaitizes, have the preamble, and generates pdf

--- src/latexUtils/latexString.ts
+++ src/latexUtils/latexString.ts
@@ -1,7 +1,138 @@
 
+// This function return the Preamble for each item of the resume. (e.g Experence)
+// This is important b.c each document can compile and create a latex pdf. 
+export function getLatexPreamble(): string {
+    return `
+        \\documentclass[letterpaper,11pt]{article}
+        
+        \\usepackage{latexsym}
+        \\usepackage[empty]{fullpage}
+        \\usepackage{titlesec}
+        \\usepackage{marvosym}
+        \\usepackage[usenames,dvipsnames]{color}
+        \\usepackage{verbatim}
+        \\usepackage{enumitem}
+        \\usepackage[hidelinks]{hyperref}
+        \\usepackage{fancyhdr}
+        \\usepackage[english]{babel}
+        \\usepackage{tabularx}
+        \\input{glyphtounicode}
+        
+        %----------FONT OPTIONS----------
+        % sans-serif
+        % \\usepackage[sfdefault]{FiraSans}
+        % \\usepackage[sfdefault]{roboto}
+        % \\usepackage[sfdefault]{noto-sans}
+        % \\usepackage[default]{sourcesanspro}
+        
+        % serif
+        % \\usepackage{CormorantGaramond}
+        % \\usepackage{charter}
+        
+        \\pagestyle{fancy}
+        \\fancyhf{} % clear all header and footer fields
+        \\fancyfoot{}
+        \\renewcommand{\\headrulewidth}{0pt}
+        \\renewcommand{\\footrulewidth}{0pt}
+        
+        % Adjust margins
+        \\addtolength{\\oddsidemargin}{-0.5in}
+        \\addtolength{\\evensidemargin}{-0.5in}
+        \\addtolength{\\textwidth}{1in}
+        \\addtolength{\\topmargin}{-.5in}
+        \\addtolength{\\textheight}{1.0in}
+        
+        \\urlstyle{same}
+        
+        \\raggedbottom
+        \\raggedright
+        \\setlength{\\tabcolsep}{0in}
+        
+        % Sections formatting
+        \\titleformat{\\section}{
+            \\vspace{-4pt}\\scshape\\raggedright\\large
+        }{}{0em}{}[\\color{black}\\titlerule \\vspace{-5pt}]
+        
+        % Ensure that generate pdf is machine readable/ATS parsable
+        \\pdfgentounicode=1
+        
+        %-------------------------
+        % Custom commands
+        \\newcommand{\\resumeItem}[1]{
+            \\item\\small{
+            {#1 \\vspace{-2pt}}
+            }
+        }
+        
+        \\newcommand{\\resumeSubheading}[4]{
+            \\vspace{-2pt}\\item
+            \\begin{tabular*}{0.97\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}
+                \\textbf{#1} & #2 \\\\
+                \\textit{\\small#3} & \\textit{\\small #4} \\\\
+            \\end{tabular*}\\vspace{-7pt}
+        }
+        
+        \\newcommand{\\resumeSubSubheading}[2]{
+            \\item
+            \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}
+                \\textit{\\small#1} & \\textit{\\small #2} \\\\
+            \\end{tabular*}\\vspace{-7pt}
+        }
+        
+        \\newcommand{\\resumeProjectHeading}[2]{
+            \\item
+            \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}
+                \\small#1 & #2 \\\\
+            \\end{tabular*}\\vspace{-7pt}
+        }
+        
+        \\newcommand{\\resumeSubItem}[1]{\\resumeItem{#1}\\vspace{-4pt}}
+        
+        \\renewcommand\\labelitemii{$\\vcenter{\\hbox{\\tiny$\\bullet$}}$}
+        
+        \\newcommand{\\resumeSubHeadingListStart}{\\begin{itemize}[leftmargin=0.15in, label={}]}
+        
+        \\newcommand{\\resumeSubHeadingListEnd}{\\end{itemize}}
+        \\newcommand{\\resumeItemListStart}{\\begin{itemize}}
+        \\newcommand{\\resumeItemListEnd}{\\end{itemize}\\vspace{-5pt}}\n
+
+            `.trim();
+  }
+
+  // This function is in charge of sanatizing our code due to Latex pickiness 
+  export function sanitize(str: string): string {
+    const symbolMap: { [key: string]: string } = {
+      '\'': '\\textquotesingle{}',
+      '"': '\\textquotedbl{}',
+      '`': '\\textasciigrave{}',
+      '^': '\\textasciicircum{}',
+      '~': '\\textasciitilde{}',
+      '<': '\\textless{}',
+      '>': '\\textgreater{}',
+      '|': '\\textbar{}',
+      '\\': '\\textbackslash{}',
+      '{': '\\{',
+      '}': '\\}',
+      '$': '\\$',
+      '&': '\\&',
+      '#': '\\#',
+      '_': '\\_',
+      '%': '\\%',
+      '/': '\\textslash{}',
+      '[': '\\textlbrack{}',
+      ']': '\\textrbrack{}',
+    };
+  
+    return Array.from(str)
+      .map(char => symbolMap[char] || char)
+      .join('');
+  }
+  
+
+
+
+
 //you don't care about user and itemName fields, but all other fields should be in the latex you generate
-
-
 /*  ------------------------------------------------- */
 /*  -------------------Heading----------------------- */
 /*  ------------------------------------------------- */
@@ -18,36 +149,38 @@ interface HeadingItem {
       items: HeadingItem[];
   }
 
-// Generating the full LaTeX for the Experience Section
-export const generateHeaderLatex= (activityObj: HeadingType) => {
-    let headerLatex = `\\begin{center}\n`;
-    headerLatex += `\\textbf{\\Huge \\scshape ${activityObj.name}} \\\\ \\vspace{1pt}\n`;
+  export const generateHeaderLatex = (activityObj: HeadingType): string => {
+    let headerLatex = getLatexPreamble();
+    headerLatex += `\\begin{document}\n\\begin{center}\n`;
+    headerLatex += `\\textbf{\\Huge \\scshape ${sanitize(activityObj.name)}} \\\\ \\vspace{1pt}\n`;
     headerLatex += `\\small `;
 
     // Iterate over each item to append it to the LaTeX string
     activityObj.items.forEach((item, index) => {
+        const sanitizedItem = sanitize(item.item);
         if (item.href) {
-        headerLatex += `\\href{${item.href}}{\\underline{${item.item}}}`;
+            headerLatex += `\\href{${item.href}}{\\underline{${sanitizedItem}}}`;
         } else {
-        headerLatex += item.item;
+            headerLatex += sanitizedItem;
         }
 
         // Add a separator if it's not the last item
         if (index < activityObj.items.length - 1) {
-        headerLatex += ` $|$ `;
+            headerLatex += ` $|$ `;
         }
     });
 
-    headerLatex += `\n\\end{center}`;
+    headerLatex += `\n\\end{center}\n\\end{document}`;
+
     return headerLatex;
-}
+};
 
 // testing the header
 
 export const fakeHeading: HeadingType = {
     user: "John",
     itemName: "DOE",
-    name: "This Is My Name",
+    name: "This Is My Name%",
     items: [
         { item: "Hello", href: null }, 
         { item: "meow", href: null }, 
@@ -72,6 +205,7 @@ export interface ExperienceType {
 
 // Generating the full LaTeX for the Experience Section
 export const generateExperienceLatex = (activityObj: ExperienceType) => {
+    
     let latexString = `\\resumeSubheading{${activityObj.title}}{${activityObj.subtitle}}{${activityObj.location}}{${activityObj.date}}
     \\resumeItemListStart
     `;
@@ -81,6 +215,7 @@ export const generateExperienceLatex = (activityObj: ExperienceType) => {
     });
 
     latexString += '\\resumeItemListEnd';
+    getLatexPreamble()
     return latexString;
 }
 
