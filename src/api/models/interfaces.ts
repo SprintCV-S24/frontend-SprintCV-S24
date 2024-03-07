12248f0a85760f34008279be4efedb0ea73c3f78
Jack Garritano <114269804+jackgarritano@users.noreply.github.com>
2024-03-06 20:00:24 -0600

    updates baseitem type

--- src/api/models/interfaces.ts
+++ src/api/models/interfaces.ts
@@ -1,12 +1,27 @@
+import { ActivitiesServerExplicitType } from "./activityModel";
+import { EducationServerExplicitType } from "./educationModel";
+import { ExperienceServerExplicitType } from "./experienceModel";
+import { HeadingServerExplicitType } from "./headingModel";
+import { ProjectServerExplicitType } from "./projectModel";
+import { SkillServerExplicitType } from "./skillModel";
+import { SectionHeadingServerType } from "./subheadingModel";
+
+export type BaseItem =
+  | ActivitiesServerExplicitType
+  | EducationServerExplicitType
+  | ExperienceServerExplicitType
+  | HeadingServerExplicitType
+  | ProjectServerExplicitType
+  | SkillServerExplicitType
+  | SectionHeadingServerType;
+
 export interface HeadingComponent {
   item: string;
   href: string | null;
 }
 
-export interface BaseItem {}
-
 // Interface for Contact details document
-export interface HeadingsType extends BaseItem {
+export interface HeadingsType {
   user: string;
   itemName: string;
   name: string;
@@ -14,7 +29,7 @@ export interface HeadingsType extends BaseItem {
 }
 
 // Interface for Education document
-export interface EducationType extends BaseItem {
+export interface EducationType {
   user: string;
   itemName: string;
   bullets: string[];
@@ -25,7 +40,7 @@ export interface EducationType extends BaseItem {
 }
 
 // Interface for Experience document
-export interface ExperienceType extends BaseItem  {
+export interface ExperienceType {
   user: string;
   itemName: string;
   bullets: string[];
@@ -36,7 +51,7 @@ export interface ExperienceType extends BaseItem  {
 }
 
 // Interface for Project document
-export interface ProjectsType extends BaseItem {
+export interface ProjectsType {
   user: string;
   itemName: string;
   bullets: string[];
@@ -46,7 +61,7 @@ export interface ProjectsType extends BaseItem {
 }
 
 // Interface for Skills document
-export interface SkillsType extends BaseItem {
+export interface SkillsType {
   user: string;
   itemName: string;
   title: string;
@@ -54,7 +69,7 @@ export interface SkillsType extends BaseItem {
 }
 
 // Interface for Activities document
-export interface ActivitiesType extends BaseItem {
+export interface ActivitiesType {
   user: string;
   itemName: string;
   bullets: string[];
@@ -65,15 +80,15 @@ export interface ActivitiesType extends BaseItem {
 }
 
 // Interface for Section Heading document
-export interface SectionHeadingsType extends BaseItem {
+export interface SectionHeadingsType {
   user: string;
   itemName: string;
   title: string;
 }
 
-export interface SkillsType extends BaseItem {
-	user: string;
-	itemName: string;
+export interface SkillsType {
+  user: string;
+  itemName: string;
   title: string;
   description: string;
 }
